2e965ba74ce7109756e62416776399e31876d5e9
vpia <virgil.pia@gmail.com>
2016-10-13 16:54:37 +0200

    modifié :         sstring.c

--- sstring.c
+++ sstring.c
@@ -1,117 +1,127 @@
-# include <stdlib.h>
-# include <string.h>
-# include <ctype.h>
-# include <assert.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
-# include "sstring.h"
-
-# undef NDEBUG    // FORCE ASSERT ACTIVATION
-
+#include "sstring.h"
+#undef NDEBUG // FORCE ASSERT ACTIVATION
 
 /*! Used to test the validity in asserts */
-# define ASSERT_SSTRING_OK( ss )					\
-  assert ( NULL != ss ) ;						\
-  assert ( ( 0 == ss -> length ) == ( NULL == ss -> chars ) ) 
+#define ASSERT_SSTRING_OK(ss)                                                  \
+  assert(NULL != ss);                                                          \
+  assert((0 == ss->length) == (NULL == ss->chars))
 
-/*! 
+/*!
  * Structure to store a sstring.
  * There is no room for the final \c '\0' of C-string.
  * \param length  length of the string, the number of char exactly
  * \param chars pointer to the the sequence of char
  */
 typedef struct sstring_struct {
-  unsigned int length ;
-  char * chars ;
-} sstring_struct ;
+  unsigned int length;
+  char *chars;
+} sstring_struct;
 
-
-bool sstring_is_empty ( sstring ss ) {
-  ASSERT_SSTRING_OK ( ss ) ;
-  return 0 == ss -> length ;
+bool sstring_is_empty(sstring ss) {
+  ASSERT_SSTRING_OK(ss);
+  return 0 == ss->length;
 }
 
-sstring sstring_create_empty ( void ) {
-  sstring res = malloc ( sizeof ( sstring_struct ) ) ;
-  assert ( NULL != res ) ;
-  res -> length = 0 ;
-  res -> chars = NULL ;
-  ASSERT_SSTRING_OK ( res ) ;
-  return res ;
+sstring sstring_create_empty(void) {
+  sstring res = malloc(sizeof(sstring_struct));
+  assert(NULL != res);
+  res->length = 0;
+  res->chars = NULL;
+  ASSERT_SSTRING_OK(res);
+  return res;
 }
 
-sstring sstring_create_string ( char const * st ) { 
-  return NULL ;
+sstring sstring_create_string(char const *st) {
+  assert(st != NULL);
+  sstring ch = (sstring)malloc(sizeof(struct sstring_struct));
+  assert(ch != NULL);
+  ch->length = strlen(st);
+  ch->chars = (char *)malloc(sizeof(char) * (ch->length));
+  for (int i = 0; (unsigned)i < ch->length; i++) {
+    ch->chars[i] = st[i];
+  }
+  return ch;
 }
 
-
-void sstring_destroy ( sstring * ss ) {
-  assert ( NULL != ss ) ;
-  ASSERT_SSTRING_OK ( ( * ss ) ) ;
+void sstring_destroy(sstring *ss) {
+  assert(NULL != ss);
+  ASSERT_SSTRING_OK((*ss));
   // NB free ( NULL ) is legal
-  free ( ( * ss ) -> chars ) ;
-  free ( * ss ) ;
-  * ss = NULL ;
+  free((*ss)->chars);
+  free(*ss);
+  *ss = NULL;
 }
 
-
-void sstring_print ( sstring ss ,
-		     FILE * f ) {
-  ASSERT_SSTRING_OK ( ss ) ;
-  assert ( NULL != f ) ;
-  if ( ! sstring_is_empty ( ss ) ) {
-    for ( unsigned int i = 0 ; i < ss -> length ; i ++ ) {
-      fputc ( ss -> chars [i] , f ) ;
+void sstring_print(sstring ss, FILE *f) {
+  ASSERT_SSTRING_OK(ss);
+  assert(NULL != f);
+  if (!sstring_is_empty(ss)) {
+    for (unsigned int i = 0; i < ss->length; i++) {
+      fputc(ss->chars[i], f);
     }
   }
 }
 
-
-void sstring_concatenate ( sstring ss1,
-			   sstring ss2 ) { 
-}
-
-
-sstring sstring_copy ( sstring ss ) 
-{
-  ASSERT_SSTRING_OK ( ss ) ;
-  if ( sstring_is_empty ( ss ) ) {
-    return sstring_create_empty () ;
+void sstring_concatenate(sstring ss1, sstring ss2) {
+  if (ss1->chars == NULL) {
+    ss1->length = ss2->length;
+    ss1->chars = (char *)malloc(sizeof(char) * (ss1->length));
+    for (unsigned int i = 0; i < ss1->length; i++) {
+      ss1->chars[i] = ss2->chars[i];
+    }
   } else {
-    sstring res = malloc ( sizeof ( sstring_struct ) ) ;
-    assert ( NULL != res ) ;
-    res -> length = ss -> length ;
-    res -> chars = malloc ( res -> length * sizeof ( char ) ) ;
-    assert ( NULL != res -> chars ) ;
-    for ( unsigned int i = 0 ; i < res -> length ; i ++ ) {
-      res -> chars [ i ] = ss -> chars [ i ] ;
+    int lengthss1 = sstring_get_length(ss1),
+        lengthss2 = sstring_get_length(ss2), i;
+    ss1->chars =
+        (char *)realloc(ss1->chars, sizeof(char) * (lengthss1 + lengthss2));
+    ss1->length = (lengthss1 + lengthss2);
+    for (i = 0; i < lengthss2; i++) {
+      ss1->chars[i + lengthss1] = ss2->chars[i];
     }
-    ASSERT_SSTRING_OK ( res ) ;
-    return res ;
   }
 }
 
-
-
-int sstring_compare ( sstring ss1 ,
-		      sstring ss2 ) {  
-  return 0 ;
+sstring sstring_copy(sstring ss) {
+  ASSERT_SSTRING_OK(ss);
+  if (sstring_is_empty(ss)) {
+    return sstring_create_empty();
+  } else {
+    sstring res = malloc(sizeof(sstring_struct));
+    assert(NULL != res);
+    res->length = ss->length;
+    res->chars = malloc(res->length * sizeof(char));
+    assert(NULL != res->chars);
+    for (unsigned int i = 0; i < res->length; i++) {
+      res->chars[i] = ss->chars[i];
+    }
+    ASSERT_SSTRING_OK(res);
+    return res;
+  }
 }
 
-
-
-int sstring_get_length ( sstring ss ) {
-  ASSERT_SSTRING_OK ( ss ) ;
-  return ss -> length ;
+int sstring_compare(sstring ss1, sstring ss2) {
+  ASSERT_SSTRING_OK(ss1);
+  ASSERT_SSTRING_OK(ss2);
+  int i = 0;
+  do {
+    if (ss1->chars[i] != ss2->chars[i]) {
+      return 0;
+    }
+    i++;
+  } while (((unsigned)i != ss1->length) || ((unsigned)i != ss2->length));
+  return 1;
 }
 
-
-int sstring_get_char ( sstring ss ,
-		       int i ) { 
-  return 0 ;
+int sstring_get_length(sstring ss) {
+  ASSERT_SSTRING_OK(ss);
+  return ss->length;
 }
 
+int sstring_get_char(sstring ss, int i) { return ss->chars[i]; }
 
-bool sstring_is_integer ( sstring ss ,
-			  int * n_pt ) { 
-  return false ;
-}
+bool sstring_is_integer(sstring ss, int *n_pt) { return false; }
--- term.c
+++ term.c
@@ -1,11 +1,10 @@
-# include <stdlib.h>
-# include <ctype.h>
-# include <assert.h>
- 
-# include "term.h"
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-# undef NDEBUG   // FORCE ASSERT ACTIVATION
+#include "term.h"
 
+#undef NDEBUG // FORCE ASSERT ACTIVATION
 
 /*!
  * This structure is used to have a both way linked list of terms.
@@ -14,13 +13,12 @@
  */
 typedef struct term_list_struct {
   /*! Current term / argument */
-  term t ;
+  term t;
   /*! Link to previous term / argument */
-  struct term_list_struct * previous ;
+  struct term_list_struct *previous;
   /*! Link to next term / argument */
-  struct term_list_struct * next ;
-} * term_list ;
-
+  struct term_list_struct *next;
+} * term_list;
 
 /*!
  * This structure is used to record a term.
@@ -29,134 +27,76 @@ typedef struct term_list_struct {
  */
 typedef struct term_struct {
   /*! Symbol of the term */
-  sstring symbol ;
+  sstring symbol;
   /*! Number of arguments, stored for efficiency */
-  int arity ;
+  int arity;
   /*! Father term if set then this term is an argument of the father. */
-  term father ;
+  term father;
   /*! First argument in the first link of the doubled chain. */
-  term_list argument_first ;
+  term_list argument_first;
   /*! Last argument in the first link of the doubled chain. */
-  term_list argument_last ;
-} term_struct ;
-
+  term_list argument_last;
+} term_struct;
 
 /*!
- * To test whether a \c sstring is a valid symbol. 
+ * To test whether a \c sstring is a valid symbol.
  * This means not empty, no space nor parenthesis.
  * \param symbol sstring to test the validity as an symbol.
  * \return true if \c symbol is valid as a term symbol.
  */
-static bool symbol_is_valild ( sstring const symbol ) { 
-  return false ;
+static bool symbol_is_valid(sstring const symbol) { return false; }
+
+term term_create(sstring symbol) { return NULL; }
+
+void term_destroy(term *t) {}
+
+sstring term_get_symbol(term t) {
+  assert(NULL != t);
+  return t->symbol;
 }
 
-
-term term_create ( sstring symbol ) { 
-  return NULL ;
+int term_get_arity(term t) {
+  assert(NULL != t);
+  return t->arity;
 }
 
-
-
-void term_destroy ( term * t ) { 
+term term_get_father(term t) {
+  assert(NULL != t);
+  return t->father;
 }
 
+void term_add_argument_last(term t, term a) {}
 
-sstring term_get_symbol ( term t ) { 
-  assert ( NULL != t ) ;
-  return t -> symbol ;
-}
+void term_add_argument_first(term t, term a) {}
 
+void term_add_argument_position(term t, term a, int pos) {}
 
-int term_get_arity ( term t ) {
-  assert ( NULL != t ) ;
-  return t -> arity ;
-}
+bool term_contains_symbol(term t, sstring symbol) { return false; }
 
+term term_get_argument(term t, int pos) { return NULL; }
 
-term term_get_father ( term t ) {
-  assert ( NULL != t ) ;
-  return t -> father ;
-}
+term term_extract_argument(term t, int pos) { return NULL; }
 
+term term_copy(term t) { return NULL; }
 
-void term_add_argument_last ( term t ,
-			      term a ) { 
-}
+term term_copy_translate_position(term t, term *loc) { return NULL; }
 
+void term_replace_copy(term t_destination, term t_source) {}
 
-void term_add_argument_first ( term t ,
-			       term a ) { 
-}
-
-
-void term_add_argument_position ( term t ,
-				  term a ,
-				  int pos ) { 
-}
-
-
-bool term_contains_symbol ( term t ,
-			    sstring symbol ) { 
-  return false ;
-}
-
-
-term term_get_argument ( term t ,
-			 int pos ) { 
-  return NULL ;
-}
-
-
-term term_extract_argument ( term t ,
-			     int pos ) { 
-  return NULL ;
-}
-
-
-term term_copy ( term t ) { 
-  return NULL ;
-}
-
-
-term term_copy_translate_position ( term t ,
-				    term * loc ) { 
-  return NULL ;
-}
-
-
-void term_replace_copy ( term t_destination ,
-			 term t_source ) { 
-}
-
-
-int term_compare ( term t1 ,
-		   term t2 ) { 
-  return 0 ;
-}
-
-
-
+int term_compare(term t1, term t2) { return 0; }
 
 struct term_argument_traversal_struct {
-  term_list tls ;
-} ;
- 
+  term_list tls;
+};
 
-term_argument_traversal term_argument_traversal_create ( term t ) { 
-  return NULL ;
+term_argument_traversal term_argument_traversal_create(term t) { return NULL; }
+
+void term_argument_traversal_destroy(term_argument_traversal *tt) {}
+
+bool term_argument_traversal_has_next(term_argument_traversal tt) {
+  return false;
 }
 
-
-void term_argument_traversal_destroy ( term_argument_traversal * tt ) { 
-}
-
-
-bool term_argument_traversal_has_next ( term_argument_traversal tt ) { 
-  return false ;
-}
-
-
-term term_argument_traversal_get_next ( term_argument_traversal tt ) {
-  return NULL ;
+term term_argument_traversal_get_next(term_argument_traversal tt) {
+  return NULL;
 }
