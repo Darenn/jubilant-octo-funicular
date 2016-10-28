d5bb0f2c56c1c994d1b8159900bb36e3b0329623
vpia <virgil.pia@gmail.com>
2016-10-28 18:49:51 +0200

    Rectify one memory leaks

--- valuate.c
+++ valuate.c
@@ -35,16 +35,17 @@ static void destroyVDL(variable_definition_list *ls) {
     sstring_destroy(&((*ls)->variable));
     destroyVDL(&(*ls)->next);
     free(*ls);
+    *ls = NULL;
   }
 }
 static variable_definition_list pushBackVDL(variable_definition_list ls,
                                             sstring variable, term value) {
-  variable_definition_list ils = (variable_definition_list)malloc(
+  variable_definition_list vdl = (variable_definition_list)malloc(
       sizeof(struct variable_definition_list_struct));
-  ils->value = term_copy(value);
-  ils->variable = sstring_copy(variable);
-  ils->next = ls;
-  return ils;
+  vdl->value = term_copy(value);
+  vdl->variable = sstring_copy(variable);
+  vdl->next = ls;
+  return vdl;
 }
 
 static bool term_is_set(term t) {
@@ -63,28 +64,23 @@ static term term_valuate_inner(term t, variable_definition_list var_list) {
     term variable = term_extract_argument(t, 0);
     term value = term_extract_argument(t, 0);
     term arg = term_extract_argument(t, 0);
-
     var_list = pushBackVDL(var_list, term_get_symbol(variable), value);
-
     term_destroy(&variable);
     term_destroy(&value);
-
+    term_destroy(&t);
     t = term_valuate_inner(arg, var_list);
   }
 
-  variable_definition_list tmp = var_list;
-  while (tmp != NULL) {
-    term_replace_variable(t, tmp->variable, tmp->value);
-    tmp = tmp->next;
+  variable_definition_list vdl = var_list;
+  while (vdl != NULL) {
+    term_replace_variable(t, vdl->variable, vdl->value);
+    vdl = vdl->next;
   }
-
   for (int i = 0; i < term_get_arity(t); i++) {
     term tmp = term_extract_argument(t, i);
     term arg = term_valuate_inner(tmp, var_list);
     term_add_argument_position(t, arg, i);
   }
-
-  destroyVDL(&var_list);
   return t;
 }
 
@@ -92,9 +88,9 @@ term term_valuate(term t) {
   assert(t != NULL);
   term t_copy = term_copy(t);
   ss_set = sstring_create_string(symbol_set);
-  variable_definition_list va_list = NULL;
-  t_copy = term_valuate_inner(t_copy, va_list);
-  destroyVDL(&va_list);
+  variable_definition_list var_list = NULL;
+  t_copy = term_valuate_inner(t_copy, var_list);
+  destroyVDL(&var_list);
   sstring_destroy(&ss_set);
   return t_copy;
 }
