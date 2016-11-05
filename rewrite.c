151f8335076af2c1d384015f538e168ed12dd9e2
Darenn Keller <keller.darenn@gmail.com>
2016-11-05 10:24:00 +0100

    No more memory leaks on rewrite. All work.

--- rewrite.c
+++ rewrite.c
@@ -17,11 +17,12 @@ static char const *const symbol_results = "results";
 static char const *const symbol_affectation = "affectation";
 /*! Symbol key-word for the valuation of one variable. */
 static char const *const symbol_valuation = "valuation";
+/*! Symbol key-word for error in affectations */
 static char const *const symbol_error = "ERROR";
 
 static void term_clean_arguments(term t) {
   int arity = term_get_arity(t);
-  for (size_t i = 0; i < arity; i++) {
+  for (int i = 0; i < arity; i++) {
     term arg = term_extract_argument(t, 0);
     term_destroy(&arg);
   }
@@ -36,16 +37,8 @@ static term term_create_valuation(term variable, term value) {
   return valuation;
 }
 
-static void copy_arguments_from_to(term from, term to) {
-  term_argument_traversal iterator = term_argument_traversal_create(from);
-  while (term_argument_traversal_has_next(iterator)) {
-    term_add_argument_last(
-        to, term_copy(term_argument_traversal_get_next(iterator)));
-  }
-}
-
 static bool term_contains_variable(term t) {
-  for (size_t i = 0; i < term_get_arity(t); i++) {
+  for (int i = 0; i < term_get_arity(t); i++) {
     if (term_is_variable(term_get_argument(t, i))) {
       return true;
     }
@@ -85,12 +78,11 @@ static void term_add_arg_sort_unique(term t, term arg) {
 
 term term_copy_replace_at_loc(term t, term r, term *loc) {
   if (*loc == t) {
-    return term_copy(r);
+    return r;
   }
   term new = term_create(term_get_symbol(t));
   for (int i = 0; i < term_get_arity(t); i++) {
     term arg = term_get_argument(t, i);
-    // term copyarg = term_copy(arg);
     if (*loc == arg) {
       term_add_argument_last(new, r);
     } else {
@@ -98,6 +90,13 @@ term term_copy_replace_at_loc(term t, term r, term *loc) {
     }
   }
   return new;
+}
+
+static term term_create_error() {
+  sstring error_symbol = sstring_create_string(symbol_error);
+  term error = term_create(error_symbol);
+  sstring_destroy(&error_symbol);
+  return error;
 }
 
 /*!
@@ -111,25 +110,19 @@ term term_copy_replace_at_loc(term t, term r, term *loc) {
  * accordingly.
  */
 static bool term_is_pattern(term t, term pattern, term affectation) {
-  // printf("Pattern : ");
-  //////term_print_compact(pattern, stdout);
-  // puts("\n");
-  // printf("Term : ");
-  // //////term_print_compact(t, stdout);
-  // puts("\n");
+  // If the pattern is a variable, try to affect the term as value of the
+  // pattern variable. If there is already a value affected to this variable,
+  // verify that this value is equal to the term, if not, there is an error,
+  // insert error term for later.
   if (term_is_variable(pattern)) {
     if (term_contains_symbol(affectation, term_get_symbol(pattern))) {
-      for (size_t i = 0; i < term_get_arity(affectation); i++) {
+      for (int i = 0; i < term_get_arity(affectation); i++) {
         term valuation = term_get_argument(affectation, i);
         if (sstring_compare(term_get_symbol(term_get_argument(valuation, 0)),
                             term_get_symbol(pattern)) == 0) {
           if (term_compare(term_get_argument(valuation, 1), t) != 0) {
-            // printf("trouve una val diff pour une variable");
-            sstring error_symbol = sstring_create_string(symbol_error);
-            term error = term_create(error_symbol);
-            sstring_destroy(&error_symbol);
+            term error = term_create_error();
             term_add_arg_sort_unique(affectation, error);
-            // printf("retourne faux car double affection.");
             return false;
           }
         }
@@ -139,49 +132,52 @@ static bool term_is_pattern(term t, term pattern, term affectation) {
     term_add_argument_last(affectation, newValuation);
     return true;
   }
+  // If the arity of the term is different from the arity of pattern, it's false
   if (term_get_arity(t) != term_get_arity(pattern)) {
     term_clean_arguments(affectation);
     return false;
   }
+  // If the pattern and the term are exactly same, it's true
   if (term_compare(t, pattern) == 0) {
-    // printf("Retourne true car le term est pareil que le pattern");
     return true;
   }
+
+  // In case there are not identical but with the same symbol, check for each
+  // arguments of pattern, if
+  // it's a pattern of each arguments of the term. (for example with variables)
   if (sstring_compare(term_get_symbol(pattern), term_get_symbol(t)) == 0) {
     term_argument_traversal ttraversal = term_argument_traversal_create(t);
     term_argument_traversal patternTraversal =
         term_argument_traversal_create(pattern);
-    bool patternFound = false;
+    bool subtermsArePatterns = true;
     while (term_argument_traversal_has_next(ttraversal)) {
-      if (!term_argument_traversal_has_next(patternTraversal)) {
-        term_clean_arguments(affectation);
-        return false;
-      }
       term current_t = term_argument_traversal_get_next(ttraversal);
       term current_pattern = term_argument_traversal_get_next(patternTraversal);
-      if (term_is_pattern(current_t, current_pattern, affectation)) {
-        patternFound = true;
-      } else {
-        return false;
+      if (!term_is_pattern(current_t, current_pattern, affectation)) {
+        subtermsArePatterns = false;
+        break;
       }
     }
-    if (patternFound) {
-      sstring error_symbol = sstring_create_string(symbol_error);
-      if (term_contains_symbol(affectation, error_symbol)) {
-        term_clean_arguments(affectation);
-        return false;
-      } else {
-        sstring_destroy(&error_symbol);
-        return true;
-      }
+    term_argument_traversal_destroy(&patternTraversal);
+    term_argument_traversal_destroy(&ttraversal);
+    if (!subtermsArePatterns)
+      return false;
+
+    // We arrive here if we found that all arguments are also patterns or if the
+    // pattern does not seem to match the term.
+    // Now we want to check if there are any errors in affectations
+    sstring error_symbol = sstring_create_string(symbol_error);
+    if (term_contains_symbol(affectation, error_symbol)) {
+      term_clean_arguments(affectation);
+      sstring_destroy(&error_symbol);
+      return false;
+    } else {
+      sstring_destroy(&error_symbol);
+      return true;
     }
   }
-  if (term_compare(t, pattern) != 0) {
-    // printf("Retourne faux car le pattern est différent du terme");
-    return false;
-  }
-  // printf("Retourne true pas défault");
-  return true;
+  // The pattern does not match
+  return false;
 }
 
 static term term_create_result() {
@@ -207,18 +203,9 @@ static term term_create_result() {
 static void term_rewrite_rule(term t_whole, term t_current, term pattern,
                               term replace, term results) {
   term affectation = term_create_affectation();
-  // If the term is a pattern, replace the variables in it and add the
-  // possibility to results.
+  // If the term is a pattern,
   if (term_is_pattern(t_current, pattern, affectation)) {
-    // printf("it's a pattern!");
-    // term_print_compact(t_current, stdout);
-    // puts("\n");
-    // printf("of that :");
-    // term_print_compact(pattern, stdout);
-    // puts("\n");
-    // printf("i replace by:");
-    // term_print_compact(replace, stdout);
-    // puts("\n");
+    // replace the variables in it and add the possibility to results.
     term r = term_copy(replace);
     term_argument_traversal affectationTraversal =
         term_argument_traversal_create(affectation);
@@ -229,6 +216,8 @@ static void term_rewrite_rule(term t_whole, term t_current, term pattern,
                             term_get_argument(valuation, 1));
       valuesSet = true;
     }
+    term_argument_traversal_destroy(&affectationTraversal);
+
     // If the pattern contains a variable but no affectation was made, it's
     // wrong, we stop here and don't add to results
     if (term_contains_variable(pattern) && !valuesSet) {
@@ -237,14 +226,10 @@ static void term_rewrite_rule(term t_whole, term t_current, term pattern,
     // add to results the possibility
     term *loc = &t_current;
     term copy = term_copy_replace_at_loc(t_whole, r, loc);
-    // printf("copy :");
-    // term_print_compact(copy, stdout);
-    // puts("\n");
     term_add_arg_sort_unique(results, copy);
   } else {
-    // printf("not pattern");
-    // Else, the term is not a pattern, so we try to rewrite its arguments with
-    // the pattern
+    // Else, the term is not a pattern, so we try to rewrite its arguments
+    // with the pattern
     term_argument_traversal t_current_traversal =
         term_argument_traversal_create(t_current);
     while (term_argument_traversal_has_next(t_current_traversal)) {
@@ -254,6 +239,7 @@ static void term_rewrite_rule(term t_whole, term t_current, term pattern,
     }
     term_argument_traversal_destroy(&t_current_traversal);
   }
+  term_destroy(&affectation);
 }
 
 /*!
@@ -276,7 +262,6 @@ term term_rewrite(term t) {
     sstring_is_integer(term_get_symbol(firstArgument), &factor);
   }
   term termToRewrite = term_get_argument(t, term_get_arity(t) - 1);
-
   term results = term_create_result();
   term newResults = term_create_result();
   term_add_argument_last(results, term_copy(termToRewrite));
@@ -288,36 +273,32 @@ term term_rewrite(term t) {
     if (factor > 1) {
       term_argument_traversal_get_next(rewriteTraversal);
     }
-    // printf("Results :");
-    // term_print_compact(results, stdout);
     // I loop through rules
     while (term_argument_traversal_has_next(rewriteTraversal)) {
-      //   printf("azdazdazdazda");
       term rule = term_argument_traversal_get_next(rewriteTraversal);
       if (!term_argument_traversal_has_next(rewriteTraversal)) {
         // We are at the end, we stop
         break;
       }
-      //   printf("Ihyaaaaa");
       term termToReplace = term_get_argument(rule, 0);
       term replaceWith = term_get_argument(rule, 1);
       term_argument_traversal args = term_argument_traversal_create(results);
       // I Loop trough args of the results
       while (term_argument_traversal_has_next(args)) {
-        // printf("Allo?");
         term termToRewrite = term_argument_traversal_get_next(args);
         // For each args of the term to rewrite, i rewrite it
         // The possibilities are set in results
         term_rewrite_rule(termToRewrite, termToRewrite, termToReplace,
                           replaceWith, newResults);
       }
+      term_argument_traversal_destroy(&args);
     }
-    // printf("New Results :");
-    // term_print_compact(newResults, stdout);
-    // // term_destroy(results);
+    term_argument_traversal_destroy(&rewriteTraversal);
+    term_destroy(&results);
     results = term_copy(newResults);
-    // // term_destroy(newResults);
+    term_destroy(&newResults);
     newResults = term_create_result();
   }
+  term_destroy(&newResults);
   return results;
 }
